e1d3eb5c08d273a2d2612d6e404f84b14cf53394
Sergeidubouski <kukuruzka007@gmail.com>
2022-02-02 17:21:10 +0300

    anythin added

--- script2.js
+++ script2.js
@@ -2,3 +2,10 @@
 const someAdjective = "sergey";
 let myStr = "Learning to code is ";
 myStr += someAdjective;
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName;
+lastNameLength = lastName.length;
