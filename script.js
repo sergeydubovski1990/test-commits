343d6f20765bc559f2d8bccec886202e831f9957
Sergeidubouski <kukuruzka007@gmail.com>
2022-02-02 16:32:08 +0300

    create new js

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
 const myStr = "This is the start. " + "This is the end."; // Change this line
 let myStr = "This is the first sentence.";
 myStr += " This is the second sentence.";
+// Only change code below this line
+const myName = "Sergei";
+const myStr = "My name is " + myName + " I am well!";
