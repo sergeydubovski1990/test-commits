b62958a43f5d983364a7dd7c15296f39c74dcd06
Sergeidubouski <kukuruzka007@gmail.com>
2022-02-02 15:48:20 +0300

    Change this line

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
 const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
