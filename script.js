2986d3bdc150d936a59059812072f6d7d26b6a7f
Sergeidubouski <kukuruzka007@gmail.com>
2022-02-02 15:55:13 +0300

    added new

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Ch
 const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
 const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
 const myStr = "This is the start. " + "This is the end."; // Change this line
+let myStr = "This is the first sentence.";
+myStr += " This is the second sentence.";
